531001a44f74885c54cc84d34bab2d11ade64602
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-12 20:23:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,9 +3,9 @@
 
 def key_for_min_value(name_hash)
   min_value = 800000
-  name_hash.each do |key,value| 
+  name_hash.each do |key,value|
     if value < min_value
-      min_value = value 
+      min_value = value
     end
-  end 
+  end
 end
