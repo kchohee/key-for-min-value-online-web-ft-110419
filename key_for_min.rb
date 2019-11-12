3d8567385e4ec1a2daddee44e47cdc0e6900474c
Marie (Cho) Hughes <kchohee0630@gmail.com>
2019-11-12 20:27:00 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,12 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  min_v= 800000
+  m_v= 800000
   name_hash.each do |k,v|
-    if v < min_v
-      min_v = v
-      min_k = k
+    if v < m_v
+      m_v = v
+      m_k = k
     end
   end
-  return min_k
+  return m_k
 end
